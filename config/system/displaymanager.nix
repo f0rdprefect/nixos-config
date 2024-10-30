971da855296fb58f3a9f5cfb07df12caea861499
Matthias Berse <matthias@berse.xyz>
2024-10-30 13:21:27 +0100

    Add doom fire to login screen

--- config/system/displaymanager.nix
+++ config/system/displaymanager.nix
@@ -4,6 +4,9 @@ let inherit (import ../../hosts/${host}/options.nix) theKBDVariant
 theKBDLayout theSecondKBDLayout; in
 {
   services.libinput.enable = true;
+  environment.systemPackages = [ 
+    inputs.iio-hyprland.packages.${pkgs.system}.default
+  ];
   services.xserver = {
     enable = true;
     xkb = {
@@ -13,6 +16,10 @@ theKBDLayout theSecondKBDLayout; in
   };
   services.displayManager.ly = {
     enable = true;
+    settings = {
+      animation = "doom";
+      hide_borders = true;
+    };
   };
   
 }
