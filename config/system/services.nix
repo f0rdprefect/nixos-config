a363c201a242101928b2fdaf2f5a7f3a0e96d215
Matthias Berse <matthias@berse.xyz>
2024-12-06 08:04:43 +0100

    Fix font names

--- config/system/services.nix
+++ config/system/services.nix
@@ -89,7 +89,7 @@
     polarity = "dark";
     fonts = {
       monospace = {
-        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        package = pkgs.nerd-fonts.jetbrains-mono;
         name = "JetBrainsMono Nerd Font Mono";
       };
       sansSerif = {
