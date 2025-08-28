cacc2c2f13d10bfe669200f3389cb5161adb8e8f
Matthias Berse <matthias@berse.xyz>
2025-08-28 06:35:10 +0200

    Fix networkd vs networkmanager config inifinite loop

--- hosts/xin/hardware.nix
+++ hosts/xin/hardware.nix
@@ -73,11 +73,13 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  #networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
   # networking.interfaces.wwan0.useDHCP = lib.mkDefault true;
-  networking.tempAddresses = "enabled";
+  #systemd.network.wait-online.enable = lib.mkForce false;
+  #networking.tempAddresses = "enabled";
+  networking.useNetworkd = false;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.steam-hardware.enable = true;
