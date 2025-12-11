4a0e7b9104f01ff568d1796ac1164ff73941c109
Matthias Berse <matthias@berse.xyz>
2025-12-11 14:04:49 +0100

    Update & fix mime apps

--- config/home/xdg/mime-apps.nix
+++ config/home/xdg/mime-apps.nix
@@ -47,6 +47,25 @@
       "image/webp" = [ "org.nomacs.ImageLounge.desktop" ];
       "image/bmp" = [ "org.nomacs.ImageLounge.desktop" ];
       "image/svg+xml" = [ "org.nomacs.ImageLounge.desktop" ];
+
+      # Office Documents
+      "application/msword" = [ "writer.desktop" ];
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [
+        "writer.desktop"
+      ];
+      "application/vnd.ms-excel" = [ "calc.desktop" ];
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "calc.desktop" ];
+      "application/vnd.ms-powerpoint" = [ "impress.desktop" ];
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [
+        "impress.desktop"
+      ];
+      "application/rtf" = [ "writer.desktop" ];
+      "application/vnd.oasis.opendocument.text" = [ "writer.desktop" ];
+      "application/vnd.oasis.opendocument.spreadsheet" = [ "calc.desktop" ];
+      "application/vnd.oasis.opendocument.presentation" = [ "impress.desktop" ];
+      "application/vnd.oasis.opendocument.graphics" = [ "draw.desktop" ];
+      "application/vnd.oasis.opendocument.formula" = [ "math.desktop" ];
+      "application/vnd.oasis.opendocument.base" = [ "base.desktop" ];
     };
 
     associations = {
@@ -62,6 +81,25 @@
         "application/xhtml+xml" = [ "firefox.desktop" ];
         "application/x-extension-xhtml" = [ "firefox.desktop" ];
         "application/x-extension-xht" = [ "firefox.desktop" ];
+
+        # Office Documents
+        "application/msword" = [ "writer.desktop" ];
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [
+          "writer.desktop"
+        ];
+        "application/vnd.ms-excel" = [ "calc.desktop" ];
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "calc.desktop" ];
+        "application/vnd.ms-powerpoint" = [ "impress.desktop" ];
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [
+          "impress.desktop"
+        ];
+        "application/rtf" = [ "writer.desktop" ];
+        "application/vnd.oasis.opendocument.text" = [ "writer.desktop" ];
+        "application/vnd.oasis.opendocument.spreadsheet" = [ "calc.desktop" ];
+        "application/vnd.oasis.opendocument.presentation" = [ "impress.desktop" ];
+        "application/vnd.oasis.opendocument.graphics" = [ "draw.desktop" ];
+        "application/vnd.oasis.opendocument.formula" = [ "math.desktop" ];
+        "application/vnd.oasis.opendocument.base" = [ "base.desktop" ];
       };
 
       removed = {
