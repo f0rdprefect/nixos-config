ebf0520450d85a918ad7a786b4ce887a25827d59
Matthias Berse <matthias@berse.xyz>
2026-04-07 08:40:41 +0200

    Fine tune config

--- hosts/cw-0262/configuration.nix
+++ hosts/cw-0262/configuration.nix
@@ -96,7 +96,52 @@
     pkgs.btrfs-progs
     pkgs.tmux
     pkgs.pv
+    pkgs.cifs-utils
+    pkgs.keyutils
   ];
+
+  #  fileSystems."/home/matt/work/n" = {
+  #    device = "//office.raith.de/net";
+  #    fsType = "cifs";
+  #    options =
+  #      let
+  #        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  #      in
+  #      [
+  #        "${automount_opts},credentials=/home/matt/work/.smbcredentials,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,vers=3.0"
+  #        "nofail"
+  #      ];
+  #  };
+  system.activationScripts.symlink-requestkey = ''
+    if [ ! -d /sbin ]; then
+      mkdir /sbin
+    fi
+    ln -sfn /run/current-system/sw/bin/request-key /sbin/request-key
+  '';
+  # request-key expects a configuration file under /etc
+  environment.etc."request-key.conf" = {
+    text =
+      let
+        upcall = "${pkgs.cifs-utils}/bin/cifs.upcall";
+        keyctl = "${pkgs.keyutils}/bin/keyctl";
+      in
+      ''
+        #OP     TYPE          DESCRIPTION  CALLOUT_INFO  PROGRAM
+        # -t is required for DFS share servers...
+        create  cifs.spnego   *            *             ${upcall} -t %k
+        create  dns_resolver  *            *             ${upcall} %k
+        # Everything below this point is essentially the default configuration,
+        # modified minimally to work under NixOS. Notably, it provides debug
+        # logging.
+        create  user          debug:*      negate        ${keyctl} negate %k 30 %S
+        create  user          debug:*      rejected      ${keyctl} reject %k 30 %c %S
+        create  user          debug:*      expired       ${keyctl} reject %k 30 %c %S
+        create  user          debug:*      revoked       ${keyctl} reject %k 30 %c %S
+        create  user          debug:loop:* *             |${pkgs.coreutils}/bin/cat
+        create  user          debug:*      *             ${pkgs.keyutils}/share/keyutils/request-key-debug.sh %k %d %c %S
+        negate  *             *            *             ${keyctl} negate %k 30 %S
+      '';
+  };
   programs = {
     nano.enable = false;
     ydotool.enable = true;
